e2f69e5a9f3d17c9eb963c264591daaf9a6271f9
PottierLoic <loic.pottier@live.fr>
2026-04-03 17:43:09 +0200

    feat(backend): add backend factory

---
+++ backend/backend_factory.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdexcept>
+#include <memory>
+
+#include "backend.hpp"
+#include "cpu_single_threaded.hpp"
+
+std::unique_ptr<IFractalBackend> make_backend(Backend backend) {
+    switch(backend) {
+        case Backend::CpuSingleThreaded: return std::make_unique<CpuSingleThreaded>();
+//        case Backend::CpuMultiThreaded: return std::make_unique<xxx>();
+//        case Backend::CpuTiled: return std::make_unique<xxx>();
+//        case Backend::Gpu: return std::make_unique<xxx>();
+//        case Backend::CpuArbitraryPrecision: return std::make_unique<xxx>();
+        default: throw std::runtime_error("Unknown backend");
+    }
+}
