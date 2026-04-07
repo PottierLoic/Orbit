b3b11c74379eece619d67072051024cea39ef1c9
Loïc Pottier <loic.pottier@live.fr>
2026-04-07 15:59:09 +0200

    refactor(tests): add missing max_iteration parameter to apply_palette call

--- tests/color/test_palette.cpp
+++ tests/color/test_palette.cpp
@@ -10,6 +10,6 @@ TEST_CASE("RGBA vector size is equal to PixelValue vector size") {
     Palette palette;
     palette.colors = {RGBA{0, 0, 0, 255}};
     palette.cycle_length = 1;
-    std::vector<RGBA> colors = apply_palette(palette, pixels, false);
+    std::vector<RGBA> colors = apply_palette(palette, pixels, false, 100);
     CHECK(pixels.size() == colors.size());
 }
